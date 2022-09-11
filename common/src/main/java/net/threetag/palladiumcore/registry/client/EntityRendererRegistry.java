8f6b3ce02aec3969d6dafab848229676f0f9e0f5
Lucraft <lucasbandermann@gmail.com>
2022-09-11 17:40:09 +0200

    Added method for adding render layers to entities

--- common/src/main/java/net/threetag/palladiumcore/registry/client/EntityRendererRegistry.java
+++ common/src/main/java/net/threetag/palladiumcore/registry/client/EntityRendererRegistry.java
@@ -6,9 +6,13 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 @Environment(EnvType.CLIENT)
@@ -22,6 +26,19 @@ public class EntityRendererRegistry {
     @ExpectPlatform
     public static void registerModelLayer(ModelLayerLocation location, Supplier<LayerDefinition> definitionSupplier) {
         throw new AssertionError();
+    }
+
+    @ExpectPlatform
+    public static void addRenderLayer(Predicate<EntityType<?>> entityType, Function<RenderLayerParent<?, ?>, RenderLayer<?, ?>> renderLayer) {
+        throw new AssertionError();
+    }
+
+    public static void addRenderLayer(Supplier<EntityType<?>> entityType, Function<RenderLayerParent<?, ?>, RenderLayer<?, ?>> renderLayer) {
+        addRenderLayer(type -> type == entityType.get(), renderLayer);
+    }
+
+    public static void addRenderLayerToPlayer(Function<RenderLayerParent<?, ?>, RenderLayer<?, ?>> renderLayer) {
+        addRenderLayer(type -> type == EntityType.PLAYER, renderLayer);
     }
 
 }
--- fabric/src/main/java/net/threetag/palladiumcore/registry/client/fabric/EntityRendererRegistryImpl.java
+++ fabric/src/main/java/net/threetag/palladiumcore/registry/client/fabric/EntityRendererRegistryImpl.java
@@ -1,5 +1,6 @@
 package net.threetag.palladiumcore.registry.client.fabric;
 
+import com.mojang.datafixers.util.Pair;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
@@ -7,13 +8,21 @@ import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 @Environment(EnvType.CLIENT)
 public class EntityRendererRegistryImpl {
+
+    public static final List<Pair<Predicate<EntityType<?>>, Function<RenderLayerParent<?, ?>, RenderLayer<?, ?>>>> RENDER_LAYERS = new ArrayList<>();
 
     public static <T extends Entity> void register(Supplier<? extends EntityType<? extends T>> typeSupplier, EntityRendererProvider<T> provider) {
         EntityRendererRegistry.register(typeSupplier.get(), provider);
@@ -21,6 +30,10 @@ public class EntityRendererRegistryImpl {
 
     public static void registerModelLayer(ModelLayerLocation location, Supplier<LayerDefinition> definitionSupplier) {
         EntityModelLayerRegistry.registerModelLayer(location, definitionSupplier::get);
+    }
+
+    public static void addRenderLayer(Predicate<EntityType<?>> entityType, Function<RenderLayerParent<?, ?>, RenderLayer<?, ?>> renderLayer) {
+        RENDER_LAYERS.add(Pair.of(entityType, renderLayer));
     }
 
 }
