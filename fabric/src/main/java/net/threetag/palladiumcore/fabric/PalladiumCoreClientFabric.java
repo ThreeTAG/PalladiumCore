ae1d4decb73058c4de1551a262d1f42455ced72d
Lucraft <lucasbandermann@gmail.com>
2022-09-11 17:23:27 +0200

    Added missing stuff for extended entity spawn data

--- fabric/src/main/java/net/threetag/palladiumcore/fabric/PalladiumCoreClientFabric.java
+++ fabric/src/main/java/net/threetag/palladiumcore/fabric/PalladiumCoreClientFabric.java
@@ -2,12 +2,14 @@ package net.threetag.palladiumcore.fabric;
 
 import net.fabricmc.api.ModInitializer;
 import net.threetag.palladiumcore.PalladiumCoreClient;
+import net.threetag.palladiumcore.network.fabric.SpawnEntityPacket;
 
 public class PalladiumCoreClientFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
         PalladiumCoreClient.init();
+        SpawnEntityPacket.Client.register();
     }
     
 }
