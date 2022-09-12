1cc8f714f4d413cd18339f72164a5033b6f6dcd8
Lucraft <lucasbandermann@gmail.com>
2022-09-12 19:17:25 +0200

    Added missing mixin for livng hurt event

--- fabric/src/main/java/net/threetag/palladiumcore/mixin/fabric/PlayerMixin.java
+++ fabric/src/main/java/net/threetag/palladiumcore/mixin/fabric/PlayerMixin.java
@@ -1,6 +1,7 @@
 package net.threetag.palladiumcore.mixin.fabric;
 
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.threetag.palladiumcore.event.LivingEntityEvents;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,6 +18,16 @@ public class PlayerMixin {
             cancellable = true)
     private void die(DamageSource source, CallbackInfo ci) {
         if (LivingEntityEvents.DEATH.invoker().livingEntityDeath((Player) (Object) this, source).cancelsEvent()) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(at = @At("HEAD"),
+            method = "actuallyHurt",
+            cancellable = true)
+    private void actuallyHurt(DamageSource pDamageSource, float pDamageAmount, CallbackInfo ci) {
+        var entity = (LivingEntity) (Object) this;
+        if (!entity.isInvulnerableTo(pDamageSource) && LivingEntityEvents.HURT.invoker().livingEntityHurt(entity, pDamageSource, pDamageAmount).cancelsEvent()) {
             ci.cancel();
         }
     }
