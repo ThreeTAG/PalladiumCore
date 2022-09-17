a668149b3de6fcac22865bbcefba7bf55989527a
Lucraft <lucasbandermann@gmail.com>
2022-09-17 17:19:08 +0200

    Made DeferredRegister iterable

--- common/src/main/java/net/threetag/palladiumcore/registry/DeferredRegister.java
+++ common/src/main/java/net/threetag/palladiumcore/registry/DeferredRegister.java
@@ -3,17 +3,25 @@ package net.threetag.palladiumcore.registry;
 import dev.architectury.injectables.annotations.ExpectPlatform;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.function.Supplier;
 
-public abstract class DeferredRegister<T> {
+public abstract class DeferredRegister<T> implements Iterable<RegistrySupplier<T>> {
 
     public abstract void register();
 
     public abstract <R extends T> RegistrySupplier<R> register(String id, Supplier<R> supplier);
 
     public abstract Collection<RegistrySupplier<T>> getEntries();
+
+    @NotNull
+    @Override
+    public Iterator<RegistrySupplier<T>> iterator() {
+        return this.getEntries().iterator();
+    }
 
     @ExpectPlatform
     public static <T> DeferredRegister<T> create(String modid, ResourceKey<? extends Registry<T>> resourceKey) {
