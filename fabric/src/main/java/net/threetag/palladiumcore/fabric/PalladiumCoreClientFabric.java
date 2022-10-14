6ae6b66906f6c35bff3eaa8b0b8516c6296410a0
Lucraft <lucasbandermann@gmail.com>
2022-10-14 23:30:36 +0200

    Improved when setup events are called on Fabric side

--- fabric/src/main/java/net/threetag/palladiumcore/fabric/PalladiumCoreClientFabric.java
+++ fabric/src/main/java/net/threetag/palladiumcore/fabric/PalladiumCoreClientFabric.java
@@ -1,6 +1,7 @@
 package net.threetag.palladiumcore.fabric;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.Minecraft;
 import net.threetag.palladiumcore.PalladiumCoreClient;
@@ -13,9 +14,11 @@ public class PalladiumCoreClientFabric implements ClientModInitializer {
     public void onInitializeClient() {
         PalladiumCoreClient.init();
         SpawnEntityPacket.Client.register();
-        LifecycleEvents.SETUP.invoker().run();
-        LifecycleEvents.CLIENT_SETUP.invoker().run();
 
+        ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
+            LifecycleEvents.SETUP.invoker().run();
+            LifecycleEvents.CLIENT_SETUP.invoker().run();
+        });
         ClientTickEvents.START_CLIENT_TICK.register(instance -> net.threetag.palladiumcore.event.ClientTickEvents.CLIENT_PRE.invoker().clientTick(instance));
         ClientTickEvents.END_CLIENT_TICK.register(instance -> net.threetag.palladiumcore.event.ClientTickEvents.CLIENT_POST.invoker().clientTick(instance));
         ClientTickEvents.START_WORLD_TICK.register(instance -> net.threetag.palladiumcore.event.ClientTickEvents.CLIENT_LEVEL_PRE.invoker().clientLevelTick(Minecraft.getInstance(), instance));
--- fabric/src/main/java/net/threetag/palladiumcore/fabric/PalladiumCoreServerFabric.java
+++ fabric/src/main/java/net/threetag/palladiumcore/fabric/PalladiumCoreServerFabric.java
@@ -1,12 +1,13 @@
 package net.threetag.palladiumcore.fabric;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.threetag.palladiumcore.event.LifecycleEvents;
 
 public class PalladiumCoreServerFabric implements DedicatedServerModInitializer {
 
     @Override
     public void onInitializeServer() {
-        LifecycleEvents.SETUP.invoker().run();
+        ServerLifecycleEvents.SERVER_STARTING.register(server -> LifecycleEvents.SETUP.invoker().run());
     }
 }
