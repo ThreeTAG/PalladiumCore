954baa5c776e21bbb2cc8e085d25dc83a3699d05
Lucraft <lucasbandermann@gmail.com>
2022-12-14 17:01:01 +0100

    1.19.3 update

--- fabric/src/main/java/net/threetag/palladiumcore/registry/fabric/CreativeModeTabRegistryImpl.java
+++ fabric/src/main/java/net/threetag/palladiumcore/registry/fabric/CreativeModeTabRegistryImpl.java
@@ -1,16 +1,104 @@
 package net.threetag.palladiumcore.registry.fabric;
 
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
+import net.threetag.palladiumcore.registry.CreativeModeTabRegistry;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class CreativeModeTabRegistryImpl {
 
-    public static CreativeModeTab create(ResourceLocation id, Supplier<ItemStack> icon) {
-        return FabricItemGroupBuilder.build(id, icon);
+    public static Supplier<CreativeModeTab> create(ResourceLocation id, Consumer<CreativeModeTab.Builder> builderConsumer) {
+        CreativeModeTab.Builder builder = FabricItemGroup.builder(id);
+        builderConsumer.accept(builder);
+        return builder::build;
+    }
+
+    public static void addToTab(Supplier<CreativeModeTab> tab, Consumer<CreativeModeTabRegistry.ItemGroupEntries> entriesConsumer) {
+        ItemGroupEvents.modifyEntriesEvent(tab.get()).register(entries -> entriesConsumer.accept(new ItemGroupEntriesWrapper(entries)));
+    }
+
+    @SuppressWarnings("UnstableApiUsage")
+    private record ItemGroupEntriesWrapper(
+            FabricItemGroupEntries entries) implements CreativeModeTabRegistry.ItemGroupEntries {
+
+        @Override
+        public void add(ItemLike... item) {
+            for (ItemLike itemLike : item) {
+                this.entries.accept(itemLike);
+            }
+        }
+
+        @Override
+        public void add(CreativeModeTab.TabVisibility visibility, ItemLike... item) {
+            for (ItemLike itemLike : item) {
+                this.entries.accept(itemLike, visibility);
+            }
+        }
+
+        @Override
+        public void addAfter(ItemLike afterLast, ItemLike... item) {
+            this.entries.addAfter(afterLast, item);
+        }
+
+        @Override
+        public void addAfter(ItemLike afterLast, CreativeModeTab.TabVisibility visibility, ItemLike... item) {
+            Collection<ItemStack> list = Arrays.stream(item).map(i -> i.asItem().getDefaultInstance()).toList();
+            this.entries.addAfter(afterLast, list, visibility);
+        }
+
+        @Override
+        public void addBefore(ItemLike beforeFirst, ItemLike... item) {
+            this.entries.addBefore(beforeFirst, item);
+        }
+
+        @Override
+        public void addBefore(ItemLike beforeFirst, CreativeModeTab.TabVisibility visibility, ItemLike... item) {
+            Collection<ItemStack> list = Arrays.stream(item).map(i -> i.asItem().getDefaultInstance()).toList();
+            this.entries.addBefore(beforeFirst, list, visibility);
+        }
+
+        @Override
+        public void add(ItemStack... stack) {
+            for (ItemStack itemStack : stack) {
+                this.entries.accept(itemStack);
+            }
+        }
+
+        @Override
+        public void add(CreativeModeTab.TabVisibility visibility, ItemStack... stack) {
+            for (ItemStack itemStack : stack) {
+                this.entries.accept(itemStack, visibility);
+            }
+        }
+
+        @Override
+        public void addAfter(ItemLike afterLast, ItemStack... item) {
+            this.entries.addAfter(afterLast, item);
+        }
+
+        @Override
+        public void addAfter(ItemLike afterLast, CreativeModeTab.TabVisibility visibility, ItemStack... item) {
+            this.entries.addAfter(afterLast, Arrays.asList(item), visibility);
+        }
+
+        @Override
+        public void addBefore(ItemLike beforeFirst, ItemStack... item) {
+            this.entries.addBefore(beforeFirst, item);
+        }
+
+        @Override
+        public void addBefore(ItemLike beforeFirst, CreativeModeTab.TabVisibility visibility, ItemStack... item) {
+            this.entries.addBefore(beforeFirst, Arrays.asList(item), visibility);
+        }
     }
 
 }
--- fabric/src/main/java/net/threetag/palladiumcore/registry/fabric/DeferredRegisterImpl.java
+++ fabric/src/main/java/net/threetag/palladiumcore/registry/fabric/DeferredRegisterImpl.java
@@ -1,9 +1,8 @@
 package net.threetag.palladiumcore.registry.fabric;
 
-import com.google.common.base.MoreObjects;
 import com.google.common.collect.ImmutableList;
 import net.minecraft.core.Registry;
-import net.minecraft.data.BuiltinRegistries;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
@@ -32,7 +31,7 @@ public class DeferredRegisterImpl {
 
         public Impl(String modid, ResourceKey<? extends Registry<T>> resourceKey) {
             this.modid = modid;
-            this.registry = (Registry<T>) MoreObjects.firstNonNull(Registry.REGISTRY.get(resourceKey.location()), BuiltinRegistries.REGISTRY.get(resourceKey.location()));
+            this.registry = (Registry<T>) BuiltInRegistries.REGISTRY.get(resourceKey.location());
             this.entries = new ArrayList<>();
         }
 
@@ -46,7 +45,7 @@ public class DeferredRegisterImpl {
             ResourceLocation registeredId = new ResourceLocation(this.modid, id);
             RegistrySupplier<R> registrySupplier = new RegistrySupplier<>(registeredId, Registry.register(this.registry, registeredId, supplier.get()));
             this.entries.add((RegistrySupplier<T>) registrySupplier);
-            if(this.registry == Registry.POINT_OF_INTEREST_TYPE) {
+            if(this.registry == BuiltInRegistries.POINT_OF_INTEREST_TYPE) {
                 POI_TYPES_TO_FIX.add((RegistrySupplier) registrySupplier);
             }
             return registrySupplier;
