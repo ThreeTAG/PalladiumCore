55d322b7ffd3a4d2efefa306607a84c44df4864e
Lucraft <lucasbandermann@gmail.com>
2022-12-20 23:13:32 +0100

    Yet another particle provider register fix

--- fabric/src/main/java/net/threetag/palladiumcore/registry/client/fabric/ParticleProviderRegistryImpl.java
+++ fabric/src/main/java/net/threetag/palladiumcore/registry/client/fabric/ParticleProviderRegistryImpl.java
@@ -2,45 +2,23 @@ package net.threetag.palladiumcore.registry.client.fabric;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.Minecraft;
+import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.minecraft.client.particle.ParticleEngine;
 import net.minecraft.client.particle.ParticleProvider;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleType;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.function.Supplier;
 
-@SuppressWarnings({"rawtypes", "UnnecessaryLocalVariable", "unchecked"})
 @Environment(EnvType.CLIENT)
 public class ParticleProviderRegistryImpl {
 
-    private static final Map<Supplier<ParticleType<?>>, ParticleProvider<?>> PROVIDERS = new HashMap<>();
-    private static final Map<Supplier<ParticleType<?>>, ParticleEngine.SpriteParticleRegistration<?>> SPRITES = new HashMap<>();
-
     public static <T extends ParticleOptions> void register(Supplier<ParticleType<T>> type, ParticleProvider<T> provider) {
-        Supplier supplier = type;
-        PROVIDERS.put(supplier, provider);
+        ParticleFactoryRegistry.getInstance().register(type.get(), provider);
     }
 
     public static <T extends ParticleOptions> void register(Supplier<ParticleType<T>> type, ParticleEngine.SpriteParticleRegistration<T> registration) {
-        Supplier supplier = type;
-        SPRITES.put(supplier, registration);
-    }
-
-    public static void load() {
-        for (Map.Entry<Supplier<ParticleType<?>>, ParticleProvider<?>> entry : PROVIDERS.entrySet()) {
-            ParticleType type = entry.getKey().get();
-            ParticleProvider provider = entry.getValue();
-            Minecraft.getInstance().particleEngine.register(type, provider);
-        }
-
-        for (Map.Entry<Supplier<ParticleType<?>>, ParticleEngine.SpriteParticleRegistration<?>> entry : SPRITES.entrySet()) {
-            ParticleType type = entry.getKey().get();
-            ParticleEngine.SpriteParticleRegistration provider = entry.getValue();
-            Minecraft.getInstance().particleEngine.register(type, provider);
-        }
+        ParticleFactoryRegistry.getInstance().register(type.get(), registration::create);
     }
 
 }
