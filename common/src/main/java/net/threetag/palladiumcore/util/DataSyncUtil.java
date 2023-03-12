5899eeec2f765072bad270842cbd2e52359e0e9f
Lucraft <lucasbandermann@gmail.com>
2023-03-12 21:09:32 +0100

    Improved DataSyncUtil

--- common/src/main/java/net/threetag/palladiumcore/util/DataSyncUtil.java
+++ common/src/main/java/net/threetag/palladiumcore/util/DataSyncUtil.java
@@ -7,72 +7,58 @@ import net.threetag.palladiumcore.network.MessageS2C;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
+import java.util.function.Consumer;
 
 /**
  * Allows automatic synchronisation of entity data in all scenarios. Simply register the message, PalladiumCore will do the rest
  */
 public class DataSyncUtil {
 
-    private static final List<Function<Entity, MessageS2C>> FUNCTIONS = new ArrayList<>();
+    private static final List<EntitySync> ENTITY_SYNC = new ArrayList<>();
 
-    public static void registerMessage(Function<Entity, MessageS2C> function) {
-        FUNCTIONS.add(function);
+    public static void registerEntitySync(EntitySync entitySync) {
+        ENTITY_SYNC.add(entitySync);
     }
 
     public static void setupEvents() {
         PlayerEvents.JOIN.register(player -> {
             if (player instanceof ServerPlayer serverPlayer) {
-                for (Function<Entity, MessageS2C> function : FUNCTIONS) {
-                    var msg = function.apply(serverPlayer);
-                    if (msg != null) {
-                        msg.send(serverPlayer);
-                    }
+                for (EntitySync entitySync : ENTITY_SYNC) {
+                    entitySync.gatherMessages(serverPlayer, msg -> msg.send(serverPlayer));
                 }
             }
         });
 
         PlayerEvents.START_TRACKING.register((tracker, target) -> {
             if (tracker instanceof ServerPlayer serverPlayer) {
-                for (Function<Entity, MessageS2C> function : FUNCTIONS) {
-                    var msg = function.apply(target);
-                    if (msg != null) {
-                        msg.send(serverPlayer);
-                    }
-                }
-
-                if (target instanceof ServerPlayer targetPlayer) {
-                    for (Function<Entity, MessageS2C> function : FUNCTIONS) {
-                        var msg = function.apply(serverPlayer);
-                        if (msg != null) {
-                            msg.send(targetPlayer);
-                        }
-                    }
+                for (EntitySync entitySync : ENTITY_SYNC) {
+                    entitySync.gatherMessages(target, msg -> msg.send(serverPlayer));
                 }
             }
         });
 
         PlayerEvents.RESPAWN.register((player, endConquered) -> {
             if (player instanceof ServerPlayer serverPlayer) {
-                for (Function<Entity, MessageS2C> function : FUNCTIONS) {
-                    var msg = function.apply(player);
-                    if (msg != null) {
-                        msg.sendToTrackingAndSelf(serverPlayer);
-                    }
+                for (EntitySync entitySync : ENTITY_SYNC) {
+                    entitySync.gatherMessages(player, msg -> msg.sendToTrackingAndSelf(serverPlayer));
                 }
             }
         });
 
         PlayerEvents.CHANGED_DIMENSION.register((player, destination) -> {
             if (player instanceof ServerPlayer serverPlayer) {
-                for (Function<Entity, MessageS2C> function : FUNCTIONS) {
-                    var msg = function.apply(player);
-                    if (msg != null) {
-                        msg.sendToTrackingAndSelf(serverPlayer);
-                    }
+                for (EntitySync entitySync : ENTITY_SYNC) {
+                    entitySync.gatherMessages(player, msg -> msg.sendToTrackingAndSelf(serverPlayer));
                 }
             }
         });
+    }
+
+    @FunctionalInterface
+    public interface EntitySync {
+
+        void gatherMessages(Entity entity, Consumer<MessageS2C> consumer);
+
     }
 
 }
