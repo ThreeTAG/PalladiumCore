24f74ff9a64f5060110a88d52796e448aad6fba4
Lucas Bandermann <lucasbandermann@gmail.com>
2024-10-04 22:12:07 +0200

    Fixed registry builder on NeoForge

--- neoforge/src/main/java/net/threetag/palladiumcore/registry/neoforge/RegistryBuilderImpl.java
+++ neoforge/src/main/java/net/threetag/palladiumcore/registry/neoforge/RegistryBuilderImpl.java
@@ -1,9 +1,19 @@
 package net.threetag.palladiumcore.registry.neoforge;
 
 import net.minecraft.core.Registry;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.registries.NewRegistryEvent;
 import net.neoforged.neoforge.registries.RegistryBuilder;
+import net.threetag.palladiumcore.PalladiumCore;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@EventBusSubscriber(modid = PalladiumCore.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class RegistryBuilderImpl {
+
+    private static final List<Registry<?>> REGISTRIES = new ArrayList<>();
 
     public static <T> Registry<T> createRegistry(net.threetag.palladiumcore.registry.RegistryBuilder<T> registryBuilder) {
         RegistryBuilder<T> neoBuilder = new RegistryBuilder<>(registryBuilder.getResourceKey()).sync(registryBuilder.isSynced());
@@ -13,6 +23,13 @@ public class RegistryBuilderImpl {
         }
 
         return neoBuilder.create();
+    }
+
+    @SubscribeEvent
+    public static void registerRegistries(NewRegistryEvent event) {
+        for (Registry<?> registry : REGISTRIES) {
+            event.register(registry);
+        }
     }
 
 }
